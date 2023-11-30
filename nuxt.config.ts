e1aaec987af21b12f6f80dcdf21f4250030e3edd
LeaderbotX400 <eric@singerfamily.ca>
2023-11-29 19:01:51 -0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,72 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
+
+  experimental: {
+    typedPages: true,
+    viewTransition: true,
+  },
+
+  app: {
+    head: {
+      charset: "utf-8",
+      title: "JCS IT",
+      viewport: "width=device-width, initial-scale=1",
+      noscript: [{ innerHTML: "JCSIT requires javascript to function" }],
+    },
+  },
+
+  vue: {
+    defineModel: true,
+  },
+
+  build: {
+    transpile: ["vue-sonner"],
+  },
+
+  vite: {
+    server: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Referrer-Policy": "no-referrer",
+      },
+    },
+    esbuild: {
+      drop:
+        process.env.NODE_ENV === "development"
+          ? undefined
+          : ["console", "debugger"],
+      legalComments: "none",
+      format: "esm",
+    },
+  },
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "nuxt-icon",
+    "@vueuse/nuxt",
+    "nuxt-typed-router",
+  ],
+
+  tailwindcss: {
+    config: {
+      content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+      daisyui: {
+        themes: ["light", "dark"],
+      },
+      plugins: [
+        // require('@tailwindcss/forms'),
+        // require("@tailwindcss/typography"),
+        require("daisyui"),
+      ],
+    },
+  },
+});
