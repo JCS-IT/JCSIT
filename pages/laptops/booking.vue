e2d5411a59d24a2ae462f6902a86ea2c48a1931b
LeaderbotX400 <eric@singerfamily.ca>
2023-11-30 17:54:06 -0700

    Add event creation functionality to booking page

--- pages/laptops/booking.vue
+++ pages/laptops/booking.vue
@@ -8,6 +8,7 @@ import type {
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import FullCalendar from "@fullcalendar/vue3";
+import { blocks, carts } from "~/data/mapping";
 
 definePageMeta({
   title: "Booking",
@@ -17,11 +18,23 @@ definePageMeta({
 const router = useRouter();
 const route = useRoute("laptops-booking");
 
+const dayjs = useDayjs();
+
 const prompt = ref(false);
+
+const calendar = ref();
+
+const metaData = ref<DateSelectArg>();
 
 const mouse = ref({
   x: 0,
   y: 0,
+});
+
+const newEvent = ref({
+  name: "",
+  cart: "",
+  block: "",
 });
 
 const currentEvents = ref<EventApi[]>([]);
@@ -29,25 +42,17 @@ const currentEvents = ref<EventApi[]>([]);
 const handleDateSelect = (selectInfo: DateSelectArg) => {
   if (!selectInfo.jsEvent) return;
 
-  mouse.value.x = selectInfo.jsEvent.clientX - selectInfo.jsEvent.offsetX + 30;
-  mouse.value.y = selectInfo.jsEvent.clientY - selectInfo.jsEvent.offsetY + 30;
+  mouse.value.x = selectInfo.jsEvent.clientX - selectInfo.jsEvent.offsetX;
+  mouse.value.y = selectInfo.jsEvent.clientY - selectInfo.jsEvent.offsetY;
   prompt.value = true;
 
-  // let title = prompt("Please enter a new title for your event");
   let calendarApi = selectInfo.view.calendar;
 
   calendarApi.unselect(); // clear date selection
 
-  // if (title) {
-  //   calendarApi.addEvent({
-  //     id: Math.random().toString(36),
-  //     title,
-  //     start: selectInfo.startStr,
-  //     end: selectInfo.endStr,
-  //     allDay: selectInfo.allDay,
-  //   });
-  // }
+  metaData.value = selectInfo;
 };
+
 const handleEventClick = (clickInfo: EventClickArg) => {
   if (
     confirm(
@@ -58,15 +63,9 @@ const handleEventClick = (clickInfo: EventClickArg) => {
   }
 };
 
-const handleEvents = (events: EventApi[]) => {
-  currentEvents.value = events;
-};
-
 const calendarOptions = ref<CalendarOptions>({
-  aspectRatio: 2,
   plugins: [dayGridPlugin, interactionPlugin],
   initialView: "dayGridMonth",
-  events: [],
   nowIndicator: true,
   editable: true,
   weekends: false,
@@ -75,9 +74,41 @@ const calendarOptions = ref<CalendarOptions>({
   dayMaxEvents: true,
   select: handleDateSelect,
   eventClick: handleEventClick,
-  eventsSet: handleEvents,
   height: "50rem",
 });
+
+const addEvent = (e: SubmitEvent) => {
+  e.preventDefault();
+  if (!newEvent.value.name || !newEvent.value.cart || !newEvent.value.block)
+    return;
+
+  const cart = carts.find((c) => c.name === newEvent.value.cart);
+  // @ts-expect-error - Hack but it works
+  const block = blocks[newEvent.value.block];
+
+  if (!cart || !block) return;
+
+  const calendarApi = calendar.value?.getApi();
+
+  calendarApi.addEvent({
+    title: newEvent.value.name,
+    start: `${metaData.value?.startStr}T${block.start}`,
+    end: `${metaData.value?.startStr}T${block.end}`,
+    allDay: false,
+    extendedProps: {
+      cart,
+      block: newEvent.value.block,
+    },
+  });
+
+  prompt.value = false;
+  newEvent.value = { name: "", cart: "", block: "" };
+};
+
+const cancel = () => {
+  newEvent.value = { name: "", cart: "", block: "" };
+  prompt.value = false;
+};
 </script>
 
 <template>
@@ -86,9 +117,55 @@ const calendarOptions = ref<CalendarOptions>({
     :style="`top: ${mouse.y}px; left: ${mouse.x}px;`"
     v-if="prompt"
   >
-    <div class="card bg-base-300">
-      <div class="card-body">x: {{ mouse.x }} y: {{ mouse.y }}</div>
+    <div class="card bg-base-300 border">
+      <div class="card-body">
+        <div class="form-control gap-2">
+          <span> </span>
+          <InputText
+            placeholder="Name"
+            v-model="newEvent.name"
+            tooltip="test"
+          />
+          <InputDropdown
+            placeholder="Select a cart"
+            :options="
+              carts.map((c) => {
+                return {
+                  label: `Cart ${c.id} (${c.name.split(' ')[0]} | ${
+                    c.location
+                  })`,
+                  value: c.name,
+                };
+              })
+            "
+            v-model="newEvent.cart"
+          />
+          <InputDropdown
+            :options="[...Array(4).keys()].map((i) => `Block ${i + 1}`)"
+            placeholder="Select a block"
+            v-model="newEvent.block"
+          />
+          <div class="grid grid-cols-2 gap-2">
+            <Button
+              class="btn-success w-full"
+              @click="addEvent"
+              tooltip="Confirm"
+            >
+              <Icon name="mdi:check" />
+            </Button>
+            <Button class="btn-error w-full" @click="cancel()" tooltip="cancel">
+              <Icon name="mdi:cancel" />
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  <FullCalendar :options="calendarOptions" />
+  <FullCalendar :options="calendarOptions" ref="calendar">
+    <template #eventContent="arg">
+      <div class="fc-daygrid-event-dot"></div>
+      <span class="fc-event-time">{{ arg.event.extendedProps.block }}</span>
+      <span class="fc-event-title">{{ arg.event.title }}</span>
+    </template>
+  </FullCalendar>
 </template>
