cb72f4ee02f29fe694307512af59a0c56be21216
LeaderbotX400 <eric@singerfamily.ca>
2023-12-01 06:46:12 -0700

    Add Cart type definition

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,10 @@ export const Cart = z.object({
   id: z.number().min(1).max(10),
   name: z.string(),
   location: z.string(),
+  color: z.object({
+    bg: z.string(),
+    text: z.string(),
+  }),
 });
 
 export type Cart = z.infer<typeof Cart>;
