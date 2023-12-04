8094a1de3e48ae3bae519993fefd17a579e89686
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2023-12-04 14:49:49 -0700

    Add selectLongPressDelay option to calendarOptions

--- pages/laptops/cart-[id].vue
+++ pages/laptops/cart-[id].vue
@@ -94,6 +94,7 @@ const calendarOptions = ref<CalendarOptions>({
   eventClick: handleEventClick,
   height: "50rem",
   windowResizeDelay: 0,
+  selectLongPressDelay: 200,
 });
 
 const addEvent = (e: SubmitEvent) => {
@@ -145,66 +146,69 @@ const clear = () => {
 
 <template>
   <div class="relative w-full">
-    <div
-      class="absolute z-50 duration-200 ease-in-out transition-all"
-      :style="`top: ${targetPose.y}px; left: ${targetPose.x}px;`"
-      v-if="prompts.create"
-    >
-      <div class="card bg-base-300 border" id="newEventPrompt">
-        <div class="card-body">
-          <div class="form-control gap-2">
-            <h2 class="text-2xl font-bold">New booking</h2>
-            <span>{{ dayjs(metaData?.start).format("dddd, MMMM DD") }}</span>
-            <!-- Info -->
-            <InputBasic
-              type="text"
-              placeholder="Name"
-              v-model="newEvent.name"
-            />
-            <InputBasic
-              type="number"
-              placeholder="Room Number"
-              v-model="newEvent.room"
-            />
+    <Transition>
+      <div
+        class="absolute z-50 duration-200 ease-in-out transition-all"
+        :style="`top: ${targetPose.y}px; left: ${targetPose.x}px;`"
+        v-if="prompts.create"
+      >
+        <div class="card bg-base-300 border" id="newEventPrompt">
+          <div class="card-body">
+            <div class="form-control gap-2">
+              <h2 class="text-2xl font-bold">New booking</h2>
+              <span>{{ dayjs(metaData?.start).format("dddd, MMMM DD") }}</span>
+              <!-- Info -->
+              <InputBasic
+                type="text"
+                placeholder="Name"
+                v-model="newEvent.name"
+              />
+              <InputBasic
+                type="number"
+                placeholder="Room Number"
+                v-model="newEvent.room"
+              />
 
-            <!-- Blocks -->
-            <InputDropdown
-              :options="Object.keys(blocks)"
-              placeholder="Start block"
-              v-model="newEvent.block.start"
-              @change="newEvent.block.end = newEvent.block.start"
-            />
-            <InputDropdown
-              :options="Object.keys(blocks)"
-              placeholder="End block"
-              v-model="newEvent.block.end"
-            />
-            <div class="grid grid-cols-2 gap-2">
-              <Button
-                class="btn-success w-full"
-                @click="addEvent"
-                tooltip="Confirm"
-                :disabled="
-                  !newEvent.name ||
-                  !newEvent.room ||
-                  !newEvent.block.start ||
-                  !newEvent.block.end
-                "
-              >
-                <Icon name="mdi:check" />
-              </Button>
-              <Button
-                class="btn-error w-full"
-                @click="clear()"
-                tooltip="cancel"
-              >
-                <Icon name="mdi:cancel" />
-              </Button>
+              <!-- Blocks -->
+              <InputDropdown
+                :options="Object.keys(blocks)"
+                placeholder="Start block"
+                v-model="newEvent.block.start"
+                @change="newEvent.block.end = newEvent.block.start"
+              />
+              <InputDropdown
+                :options="Object.keys(blocks)"
+                placeholder="End block"
+                v-model="newEvent.block.end"
+              />
+              <div class="grid grid-cols-2 gap-2">
+                <Button
+                  class="btn-success w-full"
+                  @click="addEvent"
+                  tooltip="Confirm"
+                  :disabled="
+                    !newEvent.name ||
+                    !newEvent.room ||
+                    !newEvent.block.start ||
+                    !newEvent.block.end
+                  "
+                >
+                  <Icon name="mdi:check" />
+                </Button>
+                <Button
+                  class="btn-error w-full"
+                  @click="clear()"
+                  tooltip="cancel"
+                >
+                  <Icon name="mdi:cancel" />
+                </Button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Transition>
+
     <FullCalendar :options="calendarOptions" ref="calendar">
       <template #eventContent="arg">
         <div class="fc-daygrid-event-dot"></div>
@@ -214,3 +218,15 @@ const clear = () => {
     </FullCalendar>
   </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  @apply duration-100 ease-in-out transition-opacity;
+}
+
+.v-enter-from,
+.v-leave-to {
+  @apply opacity-0;
+}
+</style>
