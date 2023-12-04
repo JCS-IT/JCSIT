ea019862191946c4cefc9daf316eb151c07d0d55
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2023-12-04 12:43:50 -0700

    Update mapping.ts

--- data/mapping.ts
+++ data/mapping.ts
@@ -131,7 +131,7 @@ export const carts: Cart[] = [
   {
     id: 10,
     name: "Hades Cart",
-    location: "Office",
+    location: "1601",
     color: {
       bg: "615316",
       text: "ffffff",
