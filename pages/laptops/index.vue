22089e06fd66df618ab2084414daf82f15a17c62
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2023-12-04 14:42:39 -0700

    Refactor cart location filtering and display

--- pages/laptops/index.vue
+++ pages/laptops/index.vue
@@ -1,22 +1,38 @@
 <script setup lang="ts">
-import { carts } from "@/data/mapping";
+import { CartMap } from "@/data/mapping";
 const router = useRouter();
 const route = useRoute("laptops");
+
+const locations = ref({
+  LC3: CartMap.filter((cart) => cart.location === "LC3"),
+  LC2: CartMap.filter((cart) => cart.location === "LC2"),
+  Misc: CartMap.filter(
+    (cart) => cart.location !== "LC3" && cart.location !== "LC2"
+  ),
+});
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 flex-wrap justify-center">
-    <Button
-      v-for="cart in carts"
-      :key="cart.id"
-      @click="router.push({ name: 'laptops-cart-id', params: { id: cart.id } })"
-      :style="{
-        backgroundColor: `#${cart.color.bg}`,
-        color: `#${cart.color.text}`,
-      }"
-      class="justify-start"
+  <div class="flex flex-col gap-2">
+    <div
+      class="flex flex-col gap-2 flex-wrap justify-center group-[]:"
+      v-for="(carts, location) in locations"
     >
-      {{ `Cart ${cart.id} (${cart.name.split(" ")[0]} | ${cart.location})` }}
-    </Button>
+      <div class="divider h-0">{{ location }}</div>
+      <Button
+        v-for="cart in carts"
+        :key="cart.id"
+        @click="
+          router.push({ name: 'laptops-cart-id', params: { id: cart.id } })
+        "
+        :style="{
+          backgroundColor: `#${cart.color.bg}`,
+          color: `#${cart.color.text}`,
+        }"
+        class="justify-start"
+      >
+        {{ `Cart ${cart.id} (${cart.name.split(" ")[0]} | ${cart.location})` }}
+      </Button>
+    </div>
   </div>
 </template>
