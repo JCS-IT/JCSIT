872899a180b4fdeefdc1f3a1607a296ea11d5036
LeaderbotX400 <eric@singerfamily.ca>
2023-12-05 16:20:47 -0700

    Add redirect to "/laptops" page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 const router = useRouter();
 
-const redirect = (path: string) => {
-  window.location.href = path;
-};
+definePageMeta({
+  redirect: "/laptops",
+});
 </script>
 
 <template>
-  <div class="flex flex-col max-w-2xl gap-2">
-    <Button @click="navigateTo('/laptops')"> Laptops </Button>
-    <Button @click="redirect('https://jcs-tab-tracker.web.app')" class="btn">
-      Tab Tracker
-    </Button>
+  <div class="flex flex-col gap-2 items-center">
+    <h1>Redirecting...</h1>
+    <span class="loading loading-lg" />
   </div>
 </template>
