68ae41a1ce059a913b6712c7c2f548613e1061b1
LeaderbotX400 <eric@singerfamily.ca>
2023-12-06 17:29:25 -0700

    Refactor carts.vue component to include tab functionality and improve search functionality

--- pages/admin/carts.vue
+++ pages/admin/carts.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { Laptop } from "@/types";
 
+const tab = ref(0);
+
 const generateRandomNumber = (max: number) => {
   const num = Math.floor(Math.random() * max) + 1;
   return num.toString().padStart(3, "0");
@@ -13,7 +15,7 @@ const laptops = ref<Laptop[]>(
       .padStart(2, "0")}0${generateRandomNumber(200)}`,
     cart: Math.floor(Math.random() * 3) + 1,
     exists: false,
-  }))
+  })).sort((a, b) => a.cart - b.cart)
 );
 
 const missing = computed(() => {
@@ -21,18 +23,46 @@ const missing = computed(() => {
 });
 
 const search = ref("");
+
+const filteredLaptops = computed(() => {
+  if (search.value.includes("cart:")) {
+    const cart = search.value.split("cart:")[1];
+    return laptops.value.filter((laptop) => laptop.cart == parseInt(cart));
+  } else {
+    return laptops.value.filter((laptop) =>
+      laptop.id.toLowerCase().includes(search.value.toLowerCase())
+    );
+  }
+});
 </script>
 
 <template>
+  <div role="tablist" class="tabs tabs-lifted">
+    <button
+      role="tab"
+      class="tab"
+      :class="{ 'tab-active': tab == 0 }"
+      @click="tab = 0"
+    >
+      Carts
+    </button>
+    <button
+      role="tab"
+      class="tab"
+      :class="{ 'tab-active': tab == 1 }"
+      @click="tab = 1"
+    >
+      Missing
+    </button>
+  </div>
   <div class="flex gap-2 flex-wrap">
-    <div class="flex-grow">
+    <div class="flex-grow" v-if="tab == 0">
       <input
-        class="input input-bordered mb-2"
+        class="input input-bordered mb-2 w-full"
         type="text"
         v-model="search"
         placeholder="Search"
       />
-      <h2>Carts</h2>
       <table class="table table-zebra">
         <thead>
           <tr>
@@ -42,11 +72,7 @@ const search = ref("");
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="laptop in laptops.filter((i) =>
-              i.id.toLowerCase().includes(search.toLowerCase())
-            )"
-          >
+          <tr v-for="laptop in filteredLaptops">
             <td>{{ laptop.cart }}</td>
             <td>{{ laptop.id }}</td>
             <td class="flex">
@@ -57,26 +83,36 @@ const search = ref("");
               />
             </td>
           </tr>
+          <tr v-if="filteredLaptops.length == 0">
+            <td colspan="3" class="text-center">
+              <h2>No matches</h2>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
 
-    <div v-if="missing.length > 0" class="flex-grow">
-      <h2>Missing</h2>
-      <table class="table table-zebra">
-        <thead>
-          <tr>
-            <th>Cart</th>
-            <th>ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(laptop, i) in missing">
-            <td>{{ laptop.cart }}</td>
-            <td>{{ laptop.id }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div v-if="tab == 1" class="flex-grow">
+      <div v-if="missing.length > 0">
+        <h2>Missing</h2>
+        <table class="table table-zebra">
+          <thead>
+            <tr>
+              <th>Cart</th>
+              <th>ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(laptop, i) in missing">
+              <td>{{ laptop.cart }}</td>
+              <td>{{ laptop.id }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else>
+        <h2>No missing laptops</h2>
+      </div>
     </div>
   </div>
 </template>
