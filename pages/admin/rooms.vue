1ed2412ff52cdd310061e15e6015eb792554cc01
LeaderbotX400 <eric@singerfamily.ca>
2023-12-13 21:46:17 -0700

    Add room management

--- pages/admin/rooms.vue
+++ pages/admin/rooms.vue
@@ -1,65 +1,96 @@
 <script setup lang="ts">
-import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  writeBatch,
+} from "firebase/firestore";
 import type { ConfigData } from "~/types";
+import { useToast } from "vue-toastification";
 
 definePageMeta({
   middleware: "auth",
 });
 
-const configRef = doc(useFirestore(), "global", "config");
+const rooms = ref<string[]>([]);
+const newRoom = ref("");
 
+const configRef = doc(useFirestore(), "global", "config");
 const configData = useDocument<ConfigData>(configRef);
 
-const updateConfig = async () => {
+watchEffect(() => {
+  if (configData.value?.rooms) {
+    rooms.value = configData.value.rooms;
+  }
+});
+
+const addRoom = async () => {
+  if (newRoom.value) {
+    await updateDoc(configRef, {
+      rooms: arrayUnion(newRoom.value),
+    });
+    newRoom.value = "";
+  }
+};
+
+const removeRoom = async (room: string) => {
   await updateDoc(configRef, {
-    rooms: configData.value?.rooms,
+    rooms: arrayRemove(room),
   });
 };
 
-const generateRandomNumber = (max: number) => {
-  const num = Math.floor(Math.random() * max) + 1;
-  return num.toString().padStart(3, "0");
+const updateRooms = async () => {
+  try {
+    await updateDoc(configRef, {
+      rooms: rooms.value,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
-
-console.log(
-  new Set(
-    Array.from({ length: 300 }, (i) => {
-      return `${Math.floor(Math.random() * 3) + 1}${generateRandomNumber(200)}`;
-    }),
-  ),
-);
 </script>
 <template>
   <div class="grid gap-2">
-    <div v-if="(configData?.rooms && configData.rooms.length > 0) || true">
-      <div class="w-full flex flex-wrap gap-2">
+    <div class="grid gap-3">
+      <div class="flex join items-center justify-center flex-grow">
+        <InputBasic
+          type="text"
+          class="join-item"
+          placeholder="Space Name"
+          v-model="newRoom"
+        />
+        <button
+          class="btn input-bordered btn-ghost join-item"
+          title="Add"
+          @click="addRoom()"
+        >
+          <IconCSS name="mdi:plus-circle" />
+        </button>
+      </div>
+    </div>
+    <div class="divider" />
+    <div v-if="configData?.rooms && configData.rooms.length > 0">
+      <div class="flex flex-wrap max-w-4xl gap-1">
         <div
-          v-for="(room, i) in new Set(
-            Array.from({ length: 300 }, (i) => {
-              return `${
-                Math.floor(Math.random() * 3) + 1
-              }${generateRandomNumber(200)}`;
-            }),
-          )"
+          class="flex join items-center justify-center flex-grow"
+          v-for="(room, index) in rooms"
         >
           <input
             type="text"
-            class="input input-bordered"
-            :key="room"
-            :value="room"
+            class="input input-bordered join-item"
+            v-model="rooms[index]"
+            @change.lazy="updateRooms()"
           />
-          <IconCSS name="mdi:delete" class="btn btn-ghost btn-error" />
+          <button
+            class="btn input-bordered btn-ghost join-item"
+            @click="removeRoom(room)"
+          >
+            <IconCSS name="mdi:delete" />
+          </button>
         </div>
       </div>
-      <button class="btn" @click="updateConfig">
-        <IconCSS name="mdi:content-save" />
-        <span>Save</span>
-      </button>
     </div>
     <span v-else class="text-center"> No rooms found </span>
-    <button class="btn">
-      <IconCSS name="mdi:plus-circle" />
-      <span>Add Room</span>
-    </button>
   </div>
 </template>
