f02bdcee3f13fe0dc14239569ee72c5389619fad
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2023-12-14 10:12:19 -0700

    save

--- pages/admin/carts.vue
+++ pages/admin/carts.vue
@@ -26,40 +26,33 @@ const removeCart = async (floor: Floor, cart: Cart) => {};
 <template>
   <div v-for="floor in floors.toReversed()">
     <div class="divider">{{ floor.name }}</div>
-    <div class="flex flex-wrap gap-2">
+    <div class="flex flex-wrap gap-2 justify-center max-w-3xl">
       <div
         v-for="cart in floor.carts"
         class="card card-compact border border-[var(--fallback-bc,oklch(var(--bc)/0.2))] max-w-sm"
       >
         <div class="card-body">
           <input type="text" placeholder="Cart Name" v-model="cart.name" />
-          <div class="flex gap-1">
-            <input
-              type="text"
-              placeholder="Cart Floor"
-              v-model="cart.location.floor"
-            />
-            <input
-              type="text"
-              placeholder="Cart Room"
-              v-model="cart.location.room"
-            />
-          </div>
+          <input
+            type="text"
+            placeholder="Cart Room"
+            v-model="cart.location.room"
+          />
           <input type="text" placeholder="Cart Number" v-model="cart.id" />
-          <div class="flex">
-            <input
-              type="color"
-              placeholder="Cart Color"
-              v-model="cart.color.bg"
-            />
-            <input
-              type="color"
-              placeholder="Text Color"
-              v-model="cart.color.bg"
-            />
-          </div>
+          <input type="text" placeholder="Cart Color" v-model="cart.color.bg" />
+          <input
+            type="text"
+            placeholder="Text Color"
+            v-model="cart.color.text"
+          />
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+input {
+  @apply input input-bordered input-sm;
+}
+</style>
